dcefa00fb7f977e52327f81a372c514541dbbc61
mark <spowage@ubuntu.members.linode.com>
2016-08-18 21:45:07 +0000

    new files

---
+++ android/project/build/generated/source/buildConfig/x86/debug/com/idf1j7csj4ya7d1jgudhy/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.idf1j7csj4ya7d1jgudhy;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.idf1j7csj4ya7d1jgudhy";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "x86";
+  public static final int VERSION_CODE = 14;
+  public static final String VERSION_NAME = "0.0.1";
+}
---
+++ android/project/build/intermediates/assets/armv7/debug/www/cordova_plugins.js
@@ -0,0 +1,50 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-meteor-webapp/www/webapp_local_server.js",
+        "id": "cordova-plugin-meteor-webapp.WebAppLocalServer",
+        "merges": [
+            "WebAppLocalServer"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-inappbrowser": "1.3.0",
+    "cordova-plugin-crosswalk-webview": "1.6.0",
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-wkwebview-engine": "1.0.2",
+    "cordova-plugin-meteor-webapp": "1.3.0",
+    "cordova-plugin-statusbar": "2.1.2",
+    "cordova-plugin-splashscreen": "3.2.1"
+};
+// BOTTOM OF METADATA
+});
